4a1a5dd59b568c02171e135fcf76861feb284347
RioBithub <masterofunity@gmail.com>
2024-05-29 19:28:26 +0700

    Initial commit

---
+++ app/src/main/java/com/devalvv/lafyuu/data/CategoryData.kt
@@ -0,0 +1,6 @@
+package com.deva.lafyuu.data
+
+data class CategoryData(
+    val name: String,
+    val imageId: Int
+)
---
+++ app/src/main/java/com/devalvv/lafyuu/data/ItemData.kt
@@ -0,0 +1,7 @@
+package com.deva.lafyuu.data
+
+data class ItemData(
+    var name: String,
+    var price: String,
+    var imageId: Int
+)
